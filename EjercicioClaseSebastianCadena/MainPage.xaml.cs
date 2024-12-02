f365e97a28ac64cad058059494255b39b947c84d
sebast213123 <juan.cadena.ramirez@udla.edu.ec>
2024-12-02 08:51:24 -0500

    Creacion de Boton Recargas y Mensajes

--- EjercicioClaseSebastianCadena/MainPage.xaml.cs
+++ EjercicioClaseSebastianCadena/MainPage.xaml.cs
@@ -1,22 +1,30 @@
-﻿namespace EjercicioClaseSebastianCadena
+﻿using Microsoft.Maui.Controls;
+
+namespace EjercicioClaseSebastianCadena
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
         }
 
-       
-
-        
-
         private void Boton1_Clicked(object sender, EventArgs e)
         {
-            Navigation.PushAsync(new PaginaGrid());
+            DisplayAlert("Recarga", "Recarga de celular realizada", "OK");
+        }
+
+        private void MostrarCamposEncriptados(object sender, EventArgs e)
+        {
+            string telefono = TelefonoEntry.Text;
+            string nombre = NombreEntry.Text;
+
+            string telefonoEncriptado = "xxxxx"; 
+            string nombreEncriptado = "xxxxx"; 
+
+            
+            MensajeEncriptadoLabel.Text = $"Número de teléfono encriptado: {telefonoEncriptado}\nNombre encriptado: {nombreEncriptado}";
+            MensajeEncriptadoLabel.IsVisible = true;
         }
     }
-
 }
